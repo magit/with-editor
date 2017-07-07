791f985f63abe7f10de3fbd6fd49f9efac05a285
Jonas Bernoulli <jonas@bernoul.li>
2017-07-07 23:01:25 +0200

    with-editor-locate-emacsclient: fix previous commit
    If `*-1' fails, then we have to return nil.  To make it clear
    that this is what happens, show the return value (nil) before
    the sexp-wise confusing multi-line warning text.
    
    Also remove the unnecessary `format'.

--- with-editor.el
+++ with-editor.el
@@ -108,13 +108,12 @@
 (defun with-editor-locate-emacsclient ()
   "Search for a suitable Emacsclient executable."
   (or (with-editor-locate-emacsclient-1 (with-editor-emacsclient-path) 3)
-      (display-warning 'with-editor (format "\
+      (prog1 nil (display-warning 'with-editor "\
 Cannot determine a suitable Emacsclient
 
 Determining an Emacsclient executable suitable for the
 current Emacs instance failed.  For more information
-please see https://github.com/magit/magit/wiki/Emacsclient."))
-      nil))
+please see https://github.com/magit/magit/wiki/Emacsclient."))))
 
 (defun with-editor-locate-emacsclient-1 (path depth)
   (let* ((version-lst (-take depth (split-string emacs-version "\\.")))
