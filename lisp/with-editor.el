a4e720b12a0946a271a274bbe0b45ae07f83520b
Jonas Bernoulli <jonas@bernoul.li>
2022-01-07 11:56:42 +0100

    Release version 3.1.1

--- lisp/with-editor.el
+++ lisp/with-editor.el
@@ -11,7 +11,7 @@
 ;; Homepage: https://github.com/magit/with-editor
 
 ;; Package-Requires: ((emacs "24.4"))
-;; Package-Version: 3.0.5
+;; Package-Version: 3.1.1
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
