bce8d1bf3faf5237332aada3bb0920bbbc037fe7
Jonas Bernoulli <jonas@bernoul.li>
2023-05-11 23:42:38 +0200

    Release version 3.3.0

--- lisp/with-editor.el
+++ lisp/with-editor.el
@@ -6,7 +6,7 @@
 ;; Homepage: https://github.com/magit/with-editor
 ;; Keywords: processes terminals
 
-;; Package-Version: 3.2.0.50-git
+;; Package-Version: 3.3.0
 ;; Package-Requires: ((emacs "25.1") (compat "29.1.4.1"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
