0dde0a08cd121703d001bd85a06d1fa0b3b73228
Jonas Bernoulli <jonas@bernoul.li>
2026-05-04 19:50:58 +0200

    Bump compat dependency

--- lisp/with-editor.el
+++ lisp/with-editor.el
@@ -7,7 +7,9 @@
 ;; Keywords: processes terminals
 
 ;; Package-Version: 3.5.0
-;; Package-Requires: ((emacs "26.1") (compat "30.1"))
+;; Package-Requires: (
+;;     (emacs  "26.1")
+;;     (compat "31.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
